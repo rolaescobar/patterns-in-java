31d46e18ebebf72818f7be240723eb4b3fe92e8c
Gares <rolaescobar@gmail.com>
2020-03-28 10:54:41 -0600

    Create Animal.java

---
+++ InterfaceEjemplo/src/interfaceejemplo/Animal.java
@@ -0,0 +1,11 @@
+
+package interfaceejemplo;
+
+
+public interface Animal {
+    
+    public void nombreEspecie(String nombre);
+    public void continente();
+    public void carateristica();
+}
+
