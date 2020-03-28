a18531f775160abc491eb0c3dedf7d7a0c0e1469
Gares <rolaescobar@gmail.com>
2020-03-28 10:54:54 -0600

    Create InterfaceEjemplo.java

---
+++ InterfaceEjemplo/src/interfaceejemplo/InterfaceEjemplo.java
@@ -0,0 +1,17 @@
+
+package interfaceejemplo;
+
+
+public class InterfaceEjemplo {
+
+
+    public static void main(String[] args) {
+    
+        Mafimero  aa = new Mafimero() {};
+        aa.nombreEspecie("Mamut");
+        aa.continente();
+        
+       
+    }
+    
+}
